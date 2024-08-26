5f36f7813706683284910d4cb117d35c67d0f69f
Tom <thomasjh@gmail.com>
2024-08-26 13:46:39 +0200

    Added initial sentiment analysis of sentences.

---
+++ email_sentiment_analyzer/Email_Sentiment/static/Email_Sentiment/script.js
@@ -0,0 +1,64 @@
+addEventListener("DOMContentLoaded", (event) => {
+
+    console.log("Loaded");
+
+    document.getElementById("submit").addEventListener("click", () => {submit_data()});
+    
+    document.getElementById("textInput").addEventListener("keydown", () => {
+            document.getElementById("submit").disabled = false;
+        })
+
+})
+
+async function submit_data(){
+
+    text_content = document.getElementById("textInput").value
+    
+    if(text_content == ""){
+        console.log("Please Input text before submitting")
+        return false
+    }
+
+    let response = await fetch('/analyze', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': `${document.cookie.split('=').pop()}`
+        },
+        body: JSON.stringify({
+            data: text_content,
+        })
+    });
+    try {const result = await response.json()
+     
+        let sentences = result.sentences;
+
+        let analysed_sentences = document.createElement("p")
+        analysed_sentences.id = "returned"
+          
+        for (const [key, value] of Object.entries(sentences)) {
+            const span = document.createElement("span");
+            span.textContent = `${key}`
+
+            span.classList.add(value, "analysedSpan")
+            analysed_sentences.appendChild(span) 
+        }
+
+        if(document.getElementById("prereturn")){
+            document.getElementById("prereturn").remove()}
+        
+        if(document.getElementById("returned")){
+            document.getElementById("returned").remove()}
+            
+        
+
+        document.getElementById("return").appendChild(analysed_sentences)
+
+        document.getElementById("submit").disabled = true;
+        
+    }
+    catch (error) {
+       
+        console.log("error")
+    }
+}
--- email_sentiment_analyzer/Email_Sentiment/views.py
+++ email_sentiment_analyzer/Email_Sentiment/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+
+class_dict = {
+    8: "Top",
+    7: "Top1",
+    5: "Top2",
+    3: "Top3",
+    0: "Neutral",
+    -3: "worstm3",
+    -5: "Worstm2",
+    -7: "Worstm1",
+    -8: "Worst"
+}
 
 # We need to have this set of models to tokenize our sentences.
 try:
@@ -18,11 +32,41 @@ def index(request):
     return render(request, "Email_Sentiment/index.html", {'message': message})
 
 def analyze(request):
-#     if request.method == "POST":
+    if request.method == "POST":
+        data = json.loads(request.body)
+        text_to_analyze = data.get("data")
+        tokens = nltk.tokenize.sent_tokenize(text_to_analyze)
+        analyzer = SentimentIntensityAnalyzer()
+        token_values = []
+        
+        for item in tokens:
+            value = analyzer.polarity_scores(item)['compound']
+            value = round(value, 1) * 10
+            for level in class_dict:
+                if value < level:
+                    continue
+                else:
+                    value = level
+                    break
+
+            value = class_dict[value]
+            token_values.append(value)
+        
+
+
+        token_dict = dict(zip(tokens, token_values))
+
+
+        print(token_dict)
+       
+        # print(pol_scores)
+        
+
+
 # TODO: Return OVERALL sentiment analysis (Positive, Negative, Compound, sarcasm)
 # TODO: Return sentence-by-sentence analysis for sarcasm + Positivity/Negativy
 # TODO: Define some classes to define the level of positivity and sarcasm.
 
-#         return JsonResponse({"": }, status=200)
-    pass
+        return JsonResponse({"sentences": token_dict}, status=200)
+    
 
